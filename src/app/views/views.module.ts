86c2718d18f1d28f191b095ab099bfbc992fcf42
Dan J. Olvera Villeda <olveradan@hotmail.com>
2022-06-14 14:08:44 -0500

    Tema personalizado

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -4,9 +4,11 @@ import { AngularMaterialModule } from '../shared/angular-material.module';
 import { SharedModule } from '../shared/shared.module';
 import { ViewsRoutingModule } from './views-routing.module';
 import { HomeComponent } from './home.component';
+import { HeaderComponent } from './layout/header/header.component';
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
