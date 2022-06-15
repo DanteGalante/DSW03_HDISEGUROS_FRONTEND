d29ee403f0902c0cc4759115742a6b3dc93f800b
Dan J. Olvera Villeda <olveradan@hotmail.com>
2022-06-15 17:59:15 -0500

    WIP - Se esta trabajando en el home de administrador

--- src/app/views/modules/administrador/administrador.module.ts
+++ src/app/views/modules/administrador/administrador.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { AdministradorRoutingModule } from './administrador-routing.module';
 import { HomeAdministradorComponent } from './components/home-administrador/home-administrador.component';
 import { RegistrarTrabajadorComponent } from './components/registrar-trabajador/registrar-trabajador.component';
+import { AngularMaterialModule } from 'src/app/shared/angular-material.module';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { RegistrarTrabajadorComponent } from './components/registrar-trabajador/
     RegistrarTrabajadorComponent
   ],
   imports: [
+    AngularMaterialModule,
     CommonModule,
     AdministradorRoutingModule
   ],
