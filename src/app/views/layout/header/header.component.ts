d28b95dcd9b2ea5453abfcd586336a3353c92dfe
Dan J. Olvera Villeda <olveradan@hotmail.com>
2022-06-15 15:44:51 -0500

    Modificación al componente header para que sea dinamico

--- src/app/views/layout/header/header.component.ts
+++ src/app/views/layout/header/header.component.ts
@@ -8,13 +8,68 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
+  public menuRol: any[] = [];
+  //TODO Harcodeado para ver si funcionaba
+  public rol: string = "admin";
+
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.generarMenu();
   }
 
   salir(){
     this.router.navigate(['/auth']);
+  }
+
+  generarMenu() {
+    switch (this.rol) {
+      case "admin":
+          this.menuRol = [
+            {
+              label: "Inicio"
+            },
+            {
+              label: "Registrar trabajador"
+            }
+          ];
+        break;
+      case "conductor":
+        this.menuRol = [
+          {
+            label: "Inicio"
+          },
+          {
+            label: "Levantar reporte"
+          },
+          {
+            label: "Seguros"
+          }
+        ];
+      break;
+      case "ajustador":
+        this.menuRol = [
+          {
+            label: "Inicio"
+          },
+          {
+            label: "Reportes"
+          }
+        ]
+      break;
+      case "ejecutivo":
+        this.menuRol = [
+          {
+            label: "Inicio"
+          },
+          {
+            label: "Asignar reportes"
+          }
+        ];
+      break;
+      default:
+        break;
+    }
   }
 
 }
